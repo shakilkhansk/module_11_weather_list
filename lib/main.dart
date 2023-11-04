b6f2638702e83bd3dccecc61a3926a788d3de04f
SHAKIL <shakilkhanking@gmail.com>
2023-11-04 12:11:48 +0600

    assignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,125 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:module_11/weather.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:module_11/Photo.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final String jsonString = '''[
-    {
-      "city": "New York",
-      "temperature": 20,
-      "condition": "Clear",
-      "humidity": 60,
-      "windSpeed": 5.5
-    },
-    {
-      "city": "Los Angeles",
-      "temperature": 25,
-      "condition": "Sunny",
-      "humidity": 50,
-      "windSpeed": 6.8
-    },
-    {
-      "city": "London",
-      "temperature": 15,
-      "condition": "Partly Cloudy",
-      "humidity": 70,
-      "windSpeed": 4.2
-    },
-    {
-      "city": "Tokyo",
-      "temperature": 28,
-      "condition": "Rainy",
-      "humidity": 75,
-      "windSpeed": 8.0
-    },
-    {
-      "city": "Sydney",
-      "temperature": 22,
-      "condition": "Cloudy",
-      "humidity": 55,
-      "windSpeed": 7.3
-    }
-  ]''';
-
   @override
   Widget build(BuildContext context) {
-    List<Weather> weatherData = parseWeather(jsonString);
     return MaterialApp(
-      title: 'Weather Info',
+      title: 'Photo Viewer',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Weather Information')),
-        body: ListView.separated(
-          padding: const EdgeInsets.all(8.0),
-          itemCount: weatherData.length,
+      home: PhotoList(),
+    );
+  }
+}
+
+
+
+class PhotoList extends StatefulWidget {
+  @override
+  _PhotoListState createState() => _PhotoListState();
+}
+
+class _PhotoListState extends State<PhotoList> {
+  List<Photo> photos = [];
+  bool isLoading = true;
+  String? errorMessage;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPhotos();
+  }
+
+  fetchPhotos() async {
+    try {
+      var response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+      if (response.statusCode == 200) {
+        List<dynamic> jsonPhotos = json.decode(response.body);
+        setState(() {
+          photos = jsonPhotos.map((json) => Photo.fromJson(json)).toList();
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load photos');
+      }
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+        errorMessage = e.toString();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Photos")),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: isLoading
+            ? Center(child: CircularProgressIndicator())
+            : errorMessage != null
+            ? Center(child: Text(errorMessage!))
+            : ListView.separated(
+          separatorBuilder: (context, index) => Divider(),
+          itemCount: photos.length,
           itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5.0),
-              child: ListTile(
-                title: Text(weatherData[index].city),
-                subtitle: Text(
-                  'Temperature: ${weatherData[index].temperature}°C\n'
-                      'Condition: ${weatherData[index].condition}\n'
-                      'Humidity: ${weatherData[index].humidity}%\n'
-                      'Wind Speed: ${weatherData[index].windSpeed} m/s',
+            return ListTile(
+              contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+              leading: Image.network(
+                  photos[index].thumbnailUrl,
+                  width: 50.0,
+                  height: 50.0,
+                  fit: BoxFit.cover,
                 ),
-              ),
+              title: Text(photos[index].title),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => PhotoDetail(
+                          photo: photos[index],
+                        )));
+              },
             );
           },
-          separatorBuilder: (context, index) => Divider(),
         ),
       ),
     );
   }
 }
 
+class PhotoDetail extends StatelessWidget {
+  final Photo photo;
 
-List<Weather> parseWeather(String jsonString) {
-  List<dynamic> list = jsonDecode(jsonString) as List<dynamic>;
-  return list.map((data) => Weather.fromJson(data)).toList();
+  PhotoDetail({required this.photo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(photo.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(photo.url),
+            SizedBox(height: 20),
+            Text('ID: ${photo.id}', style: TextStyle(fontWeight: FontWeight.bold)),
+            SizedBox(height: 20),
+            Text(photo.title),
+          ],
+        ),
+      ),
+    );
+  }
 }
